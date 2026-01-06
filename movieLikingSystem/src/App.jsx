1e36ce2726105dc5732c6f26dcdd17c20350bba9
Sanjai <sanjai.patronus@gmail.com>
2026-01-06 22:03:15 +0530

    added components and pages

--- movieLikingSystem/src/App.jsx
+++ movieLikingSystem/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import Home from "./pages/Home"
+import Favourites from "./pages/Favourites";
+
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route  path="/" element={<Home/>} />
+        <Route  path="/favourities" element={<Favourites/>} />
+      </Routes>
     </div>
   );
 }
